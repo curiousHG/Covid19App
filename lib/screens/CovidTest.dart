9eef836af78d8e90059b82f92a0dde4184e2fb40
harsh6372 <58566945+harsh6372@users.noreply.github.com>
2020-07-05 10:06:30 +0530

    started

---
+++ lib/screens/CovidTest.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CovidTest extends StatefulWidget {
+  @override
+  _CovidTestState createState() => _CovidTestState();
+}
+
+class _CovidTestState extends State<CovidTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hiha"),
+    );
+  }
+}
