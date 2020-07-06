c79006d10ee068b04c659314cdeacfd3a05e621d
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2020-07-06 15:54:24 +0530

    added files

---
+++ CovidTests.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CovidTest extends StatefulWidget {
+  @override
+  _CovidTestState createState() => _CovidTestState();
+}
+
+class _CovidTestState extends State<CovidTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("hehaw"),
+      ),
+      body: Text("Hiha"),
+    );
+  }
+}
